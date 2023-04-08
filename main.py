3a28682c6ad424f1ef94a84678fdce993ea0c97a
0starzyk <starzyk@student.agh.edu.pl>
2023-04-09 01:47:41 +0200

    Refactor

--- main.py
+++ main.py
@@ -49,13 +49,13 @@ if __name__ == "__main__":
     click_on_element(driver, "/html/body/div[1]/div/div[1]/div/div[5]/div/div[1]/div[2]/span/div")
 
     send_keys_to_element(driver, "/html/body/div[1]/div/div[1]/div/div[5]/div/div[1]/div[1]/div/div/div/div/div/div[2]/"
-                                 "div/div[1]/div/div[2]/div[1]/div/div[1]/input", "Karol Starzyk")
+                                 "div/div[1]/div/div[2]/div[1]/div/div[1]/input", "Bugs Bunny")
 
     click_on_element(driver, "/html/body/div[1]/div/div[1]/div/div[5]/div/div[1]/div[1]/div/div/div/div/div/div[2]/div/"
                              "div[1]/div/div[2]/div[2]/div/div[1]/ul/li[1]/ul/li[1]/div/a/div/div[1]")
 
     send_keys_to_element(driver, "/html/body/div[1]/div/div[1]/div/div[5]/div/div[1]/div[1]/div/div/div/div/div/div[2]/"
-                                 "div/div[2]/div[2]/div/div/div/div[4]/div[2]/div/div/div[1]", ".")
+                                 "div/div[2]/div[2]/div/div/div/div[4]/div[2]/div/div/div[1]", "What's up doc?")
 
     click_on_element(driver, "/html/body/div[1]/div/div[1]/div/div[5]/div/div[1]/div[1]/div/div/div/div/div/div[2]/div/"
                              "div[2]/div[2]/div/div/span[2]/div")
